235336eaeaa5015e73db9582693e414919518461
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-27 09:31:55 -0300

    Campos de seleção adicionados

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ titulo = customtkinter.CTkLabel(janela, text="Conversor de Moedas", font=("",20)
 texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem")
 texto_moeda_destino = customtkinter.CTkLabel(janela, text="Selecione a moeda de destino")
 
+campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=["USD", "BRL", "EUR", "BTC"])
+campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values=["USD", "BRL", "EUR", "BTC"])
+
 # colocar todos os elementos na tela
 titulo.pack(padx=10, pady=10)
 texto_moeda_origem.pack(padx=10, pady=10)
-
+campo_moeda_origem.pack(padx=10)
 texto_moeda_destino.pack(padx=10, pady=10)
+campo_moeda_destino.pack(padx=10)
 
 # rodar a janela
 janela.mainloop()
