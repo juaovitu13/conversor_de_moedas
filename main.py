c712d4ee6f2096faf227c2ca4bdcf27d1fa7710f
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-27 09:19:24 -0300

    Janela criada

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import customtkinter
+
+# criar e configurar a janela
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+janela = customtkinter.CTk()
+janela.geometry("500x500")
