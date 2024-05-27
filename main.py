bbb2d321fa8d4d889210b8428556c92af7827133
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-27 09:24:54 -0300

    Criando titulo e colocando na página

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ customtkinter.set_default_color_theme("dark-blue")
 janela = customtkinter.CTk()
 janela.geometry("500x500")
 
+# criar os botões, textos e outros elementos
+titulo = customtkinter.CTkLabel(janela, text="Conversor de Moedas", font=("",20))
+
+# colocar todos os elementos na tela
+titulo.pack(padx=10, pady=10)
 
 # rodar a janela
 janela.mainloop()
