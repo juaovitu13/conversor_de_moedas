85ea7bd3e63a2b98b7a1600e0885f90a89c70721
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-27 16:52:08 -0300

    Métodos criados

--- pegar_moedas.py
+++ pegar_moedas.py
@@ -1,12 +1,14 @@
 import xmltodict
 
-with open("moedas.xml", "rb") as arquivo_moedas:
-    dic_moedas = xmltodict.parse(arquivo_moedas)
+def nomes_moedas():
+    with open("moedas.xml", "rb") as arquivo_moedas:
+        dic_moedas = xmltodict.parse(arquivo_moedas)
 
-moedas = dic_moedas["xml"]
+    moedas = dic_moedas["xml"]
+    return moedas
 
+def conversoes_disponiveis():
+    with open("conversoes.xml", "rb") as arquivo_conversoes:
+            dic_conversoes = xmltodict.parse(arquivo_conversoes)
 
-with open("conversoes.xml", "rb") as arquivo_conversoes:
-        dic_conversoes = xmltodict.parse(arquivo_conversoes)
-
-conversoes = dic_conversoes["xml"]
+    conversoes = dic_conversoes["xml"]
