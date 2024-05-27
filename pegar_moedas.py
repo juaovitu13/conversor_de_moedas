0b28c314211e32a1016a179ac45b18a445bacc7a
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-27 16:47:51 -0300

    XML dos dados das conversões de moedas importado e código de leitura do arquivo feito

--- pegar_moedas.py
+++ pegar_moedas.py
@@ -2,3 +2,11 @@ import xmltodict
 
 with open("moedas.xml", "rb") as arquivo_moedas:
     dic_moedas = xmltodict.parse(arquivo_moedas)
+
+moedas = dic_moedas["xml"]
+
+
+with open("conversoes.xml", "rb") as arquivo_conversoes:
+        dic_conversoes = xmltodict.parse(arquivo_conversoes)
+
+conversoes = dic_conversoes["xml"]
