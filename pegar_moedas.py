d931b24cd0019904bcf10335daa49af490afd4b4
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-28 09:50:54 -0300

    Função para verificar as conversões disponiveis criada

--- pegar_moedas.py
+++ pegar_moedas.py
@@ -12,3 +12,13 @@ def conversoes_disponiveis():
             dic_conversoes = xmltodict.parse(arquivo_conversoes)
 
     conversoes = dic_conversoes["xml"]
+
+    dic_conversoes_disponiveis = {}
+    
+    for par_conversao in conversoes:
+        moeda_origem, moeda_destino = par_conversao.split("-")
+        if moeda_origem in dic_conversoes_disponiveis:
+            dic_conversoes_disponiveis[moeda_origem].append(moeda_destino)
+        else:
+            dic_conversoes_disponiveis[moeda_origem] = [moeda_destino]
+    return dic_conversoes_disponiveis
