1f248da05027b24463be7c49d0a758b9b7219d83
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-28 10:03:41 -0300

    método para carregar moedas destino

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ titulo = customtkinter.CTkLabel(janela, text="Conversor de Moedas", font=("",20)
 texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem")
 texto_moeda_destino = customtkinter.CTkLabel(janela, text="Selecione a moeda de destino")
 
+def carregar_moedas_destino(moeda_selecionada):
+    lista_moedas_destino = dic_conversoes_disponiveis[moeda_selecionada]
+    campo_moeda_destino.configure(values=lista_moedas_destino)
+    campo_moeda_destino.set(lista_moedas_destino[0])
+
 campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=list(dic_conversoes_disponiveis.keys()),
                                                  command=carregar_moedas_destino)
 campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values=["Selecione uma moeda de origem"])
