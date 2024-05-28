9ba9e24c363daeeaeb0da9b835f662fed4b897ca
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-28 15:08:18 -0300

    importando método pegar cotacao e reformulando o método  converter moeda

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter
 from pegar_moedas import nomes_moedas, conversoes_disponiveis
+from pegar_cotacao import pegar_cotacao_moeda
 
 # criar e configurar a janela
 customtkinter.set_appearance_mode("dark")
@@ -25,11 +26,17 @@ campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=list(dic_convers
 campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values=["Selecione uma moeda de origem"])
 
 def converter_moeda():
-    print("Converter moeda")
+    moeda_origem = campo_moeda_origem.get()
+    moeda_destino = campo_moeda_destino.get()
+    if moeda_origem and moeda_destino:
+        cotacao = pegar_cotacao_moeda(moeda_origem, moeda_destino)
+        texto_cotacao_moeda.configure(text=f"1 {moeda_origem} = {cotacao} {moeda_destino}")
 
 botao_converter = customtkinter.CTkButton(janela, text="Converter", command=converter_moeda)
 
 lista_moedas = customtkinter.CTkScrollableFrame(janela)
+
+texto_cotacao_moeda = customtkinter.CTkLabel(janela, text="")
 
 moedas_disponiveis = nomes_moedas()
 for codigo_moeda in moedas_disponiveis:
